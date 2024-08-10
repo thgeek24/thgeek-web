08565884a043a771bd8a508d34c2e0cd59937bcd
THGeek <alex24ht@hotmail>
2024-08-10 22:29:44 +0800

    feat: add Header.js, Footer.js and Button.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Button from './components/Button';
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Welcome to My Full-Stack Website</h1>
-        </header>
-        <main>
-          <p>This is the beginning of my journey to become a full-stack engineer.</p>
-        </main>
-      </div>
-  );
+    const handleClick = () => {
+        alert('Button clicked!');
+    };
+
+    return (
+        <div className="App">
+            <Header/>
+            <main className="App-main">
+                <h2>Welcome to My Full-Stack Website</h2>
+                <p>This is the beginning of my journey to become a full-stack engineer.</p>
+                <Button text="Click me!" onClick={handleClick}/>
+            </main>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
